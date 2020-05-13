ace9b888cd90b04d6e6d9993664f3092de58d662
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-05-12 20:21:57 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {saveAs} from 'file-saver';
+import { saveAs } from 'file-saver';
 import './App.css';
 
 class App extends Component {
-  state={
+  state = {
     name: '',
-    receiptId:0,
-    price1:0,
-    price2:0,
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
   }
 
-  handleChange=({target:{value, name}})=>this.setState({[name]:value})
+  handleChange = ({ target: { value, name } }) => this.setState({ [name]: value })
 
-  createAndDownloadPdf=()=>{
-    axios.post('/create-pdf',this.state)
+  createAndDownloadPdf = () => {
+    axios.post('/create-pdf', this.state)
+    .then(()=>axios.get('fetch-pdf',{responseType:'blob'}))
+    .then((res)=>{
+      const pdfBlob=new Blob([res.data],{type:'application/pdf'});
+      saveAs(pdfBlob,'newPdf.pdf');
+    })
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <input type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
-        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange}/>
-        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange}/>
-        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange}/>
+        <input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
+        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
         <button onClick={this.createAndDownloadPdf}>Download PDF</button>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -11,4 +11,18 @@ app.use(cors());
 app.use(bodyParse.urlencoded({extended:true}));
 app.use(bodyParse.json());
 
+app.post('/create-pdf',(req,res)=>{
+    pdf.create(pdfTemplate(req.body),{}).toFile('result.pdf', (err)=>{
+        if(err){
+            res.send(Promise.reject());
+        }
+
+        res.send(Promise.resolve());
+    });
+});
+
+app.get('/fetch-pdf',(req,res)=>{
+    res.sendFile(`${__dirname}/result.pdf`)
+})
+
 app.listen(port,()=>console.log(`Listening on port ${port}`));
